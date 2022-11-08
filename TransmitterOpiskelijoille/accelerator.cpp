44aa033e1fcb7ba15fb9a7a75f4b18903f2611bb
unknown <rikupaananen@live.fi>
2022-11-08 14:35:38 +0200

    Laskujen määrä valmis

--- TransmitterOpiskelijoille/accelerator.cpp
+++ TransmitterOpiskelijoille/accelerator.cpp
@@ -1,22 +1,26 @@
 #include "accelerator.h"
 #include <arduino.h>
 
-Accelerator::Accelerator()
-{
-   Serial.println("Accelerator created!");
+Accelerator::Accelerator() {
+  Serial.println("Accelerator created!");
 }
 
 
-Accelerator::~Accelerator()
-{
-   Serial.println("Accelerator deleted!");
+Accelerator::~Accelerator() {
+  Serial.println("Accelerator deleted!");
 }
 
-void Accelerator::makeMeasurement()
-{
-
+void Accelerator::makeMeasurement() {
+  m.x = analogRead(xPin);
+  m.y = analogRead(yPin);
+  m.z = analogRead(zPin);
 }
-Measurement Accelerator::getMeasurement()
-{
- 
+Measurement Accelerator::getMeasurement() {
+  return m;
+}
+
+void Accelerator::print() {
+  Serial.println(m.x);
+  Serial.println(m.y);
+  Serial.println(m.z);
 }
--- TransmitterOpiskelijoille/accelerator.h
+++ TransmitterOpiskelijoille/accelerator.h
@@ -3,24 +3,28 @@
 
 #include "messaging.h"
 
-struct Measurement
-{
+struct Measurement {
   int x;
   int y;
   int z;
 };
 
-class Accelerator
-{
+class Accelerator {
 public:
   Accelerator();
   ~Accelerator();
   void makeMeasurement();
+  void print();
   Measurement getMeasurement();
+  // Määritellään kytkentänavat kiihtyvyysanturille:
+  
+  const int xPin = A1;     // x-kanavan mittaus
+  const int yPin = A2;     // y-kanava
+  const int zPin = A3;     // z-kanava
+  int sisaanmeno = 0;
 
 private:
   Measurement m;
- 
 };
 
-#endif // ACCELERATOR_H
+#endif  // ACCELERATOR_H
